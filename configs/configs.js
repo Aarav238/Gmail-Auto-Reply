dd75165b2bacd62b1dd3fbc55ce904ba23ee9835
Aarav238 <aarav8090shukla@gmail.com>
2023-07-08 12:11:14 +0530

    added comments

--- configs/configs.js
+++ configs/configs.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
+// Load environment variables from .env file
 export const CLIENT_ID = process.env.CLIENT_ID;
 export const CLIENT_SECRET = process.env.CLIENT_SECRET;
 export const REDIRECT_URI = process.env.REDIRECT_URI;
