5f2d0cf8a0b9fcf173ec4fd9e15b20a9e3a75553
Aarav238 <aarav8090shukla@gmail.com>
2023-07-08 12:15:19 +0530

    added comments for gmail api

--- controllers/gmailApi.js
+++ controllers/gmailApi.js
@@ -1,6 +1,8 @@
 import { google } from "googleapis";
 import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN } from "../configs/configs.js";
 
+
+// Create OAuth2 client using the provided credentials
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
@@ -8,15 +10,31 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
+
+/*
+ * Get the Gmail client with authentication.
+ * returns the Gmail client object.
+*/
 export const getGmailClient = () => {
   return google.gmail({ version: "v1", auth: oAuth2Client });
 };
 
+
+/*
+ * Get the Gmail labels for the authenticated user.
+ * returns the array of Gmail labels.
+*/
 export const getGmailLabels = async () => {
   const gmail = getGmailClient();
   const res = await gmail.users.labels.list({ userId: "me" });
   return res.data.labels;
 };
+
+/*
+ * Create a Gmail label if it does not already exist.
+ * param {string} labelName - The name of the label to create.
+ * returns {string} The ID of the created or existing label.
+*/
 
 export const createLabelIfNeeded = async (labelName) => {
   const gmail = getGmailClient();
