2a62d0142c68d1644597e51267749eefc61acb57
Aarav238 <aarav8090shukla@gmail.com>
2023-07-08 12:07:32 +0530

    updated

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-import { google } from "googleapis";
-
-
-console.log("CLIENT_ID:", CLIENT_ID);
-console.log("CLIENT_SECRET:", CLIENT_SECRET);
-
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
-);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
+import { getGmailClient } from "./controllers/gmailApi.js";
+import { getEmail, sendReplyEmail } from "./controllers/email.js";
+import { createLabelIfNeeded } from "./controllers/gmailApi.js";
 
 const repliedUsers = new Set();
 
 const checkEmailsAndSendReplies = async () => {
   try {
-    const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
+    const gmail = getGmailClient();
     console.log("Gmail Bot Started.....");
     const res = await gmail.users.messages.list({
       userId: "me",
@@ -27,10 +17,7 @@ const checkEmailsAndSendReplies = async () => {
     if (messages && messages.length > 0) {
       console.log(`Total unread messages: ${messages.length}`);
       for (const message of messages) {
-        const email = await gmail.users.messages.get({
-          userId: "me",
-          id: message.id,
-        });
+        const email = await getEmail(message.id);
         const fromHeader = email.data.payload.headers.find(
           (header) => header.name.toLowerCase() === "from"
         );
@@ -60,12 +47,7 @@ const checkEmailsAndSendReplies = async () => {
         const replies = thread.data.messages.slice(1);
 
         if (replies.length === 0) {
-          await gmail.users.messages.send({
-            userId: "me",
-            requestBody: {
-              raw: await createReplyRaw(toEmail, fromEmail, subject),
-            },
-          });
+          await sendReplyEmail(toEmail, fromEmail, subject);
 
           const labelName = "GmailBot";
           await gmail.users.messages.modify({
@@ -88,41 +70,9 @@ const checkEmailsAndSendReplies = async () => {
   }
 };
 
-const createReplyRaw = async (from, to, subject) => {
-  const emailContent = `From: ${from}\nTo: ${to}\nSubject: ${subject}\n\nI am acknowledging your message and I will response as soon as possible.`;
-  const base64EncodedEmail = Buffer.from(emailContent)
-    .toString("base64")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
-
-  return base64EncodedEmail;
-};
-
-const createLabelIfNeeded = async (labelName) => {
-  const gmail = google.gmail({ version: "v1", auth: oAuth2Client });
-  const res = await gmail.users.labels.list({ userId: "me" });
-  const labels = res.data.labels;
-
-  const existingLabel = labels.find((label) => label.name === labelName);
-  if (existingLabel) {
-    return existingLabel.id;
-  }
-
-  const newLabel = await gmail.users.labels.create({
-    userId: "me",
-    requestBody: {
-      name: labelName,
-      labelListVisibility: "labelShow",
-      messageListVisibility: "show",
-    },
-  });
-
-  return newLabel.data.id;
-};
-
 const getRandomInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-console.log("Starting the gmail auto-reply bot...");
+console.log("Starting the Gmail auto-reply bot...");
 setInterval(checkEmailsAndSendReplies, getRandomInterval(45, 120) * 1000);
